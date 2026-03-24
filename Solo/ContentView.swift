56ccd3a07911bd3c29e6d39f4b2a6114d2a46705
Toichubek Pazylov <pazylovtoichubek7@gmail.com>
2026-03-24 23:02:49 +0600

    Initial Commit

---
+++ Solo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Solo
+//
+//  Created by Pazylov Toichubek on 24/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Solo/SoloApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoloApp.swift
+//  Solo
+//
+//  Created by Pazylov Toichubek on 24/3/26.
+//
+
+import SwiftUI
+
+@main
+struct SoloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
